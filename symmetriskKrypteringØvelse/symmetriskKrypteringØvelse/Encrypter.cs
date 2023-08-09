f2307707b7685b13db01dd2ba1fb9c7fedc9871e
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2023-08-09 18:26:29 +0200

    Update Encrypter.cs

--- symmetriskKrypteringØvelse/symmetriskKrypteringØvelse/Encrypter.cs
+++ symmetriskKrypteringØvelse/symmetriskKrypteringØvelse/Encrypter.cs
@@ -25,18 +25,18 @@ namespace symmetriskKrypteringØvelse
                 des.Key = key;
                 des.IV = iv;
 
-                // Opret en hukommelsesstrøm til krypteret data.
+                // Opret en memoryStream til krypteret data.
                 using (var memoryStream = new MemoryStream())
                 {
-                    // Opret en kryptostream baseret på DES-algoritmen.
+                    // Opret en Cryptostream baseret på DES-algoritmen.
                     var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(),
                         CryptoStreamMode.Write);
 
-                    // Skriv data til kryptostreamen, krypter det og skriv til hukommelsesstrømmen.
+                    // Skriv data til Cryptostream, krypter det og skriv til memoryStream.
                     cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
                     cryptoStream.FlushFinalBlock();
 
-                    // Konverter den krypterede hukommelsesstrøm til et byte-array.
+                    // Konverter den krypterede memoryStream til et byte-array.
                     retur = memoryStream.ToArray();
                 }
             }
@@ -45,57 +45,72 @@ namespace symmetriskKrypteringØvelse
             return retur;
         }
 
+        // Metode til at udføre kryptering ved hjælp af TripleDes-algoritmen.
         public byte[] TripleDes(byte[] dataToEncrypt, byte[] key, byte[] iv)
         {
             byte[] retur;
+
+            // Opret en instans af TripleDES-krypteringsalgoritmen.
             using (var tripledes = TripleDES.Create())
             {
+                // Konfigurer krypteringsindstillingerne.
                 tripledes.Mode = CipherMode.CBC;
                 tripledes.Padding = PaddingMode.PKCS7;
 
                 tripledes.Key = key;
                 tripledes.IV = iv;
 
+                // Opret en memoryStream til krypteret data.
                 using (var memoryStream = new MemoryStream())
                 {
+                    // Opret en cryptoStream baseret på TripleDES-algoritmen.
                     var cryptoStream = new CryptoStream(memoryStream, tripledes.CreateEncryptor(),
                         CryptoStreamMode.Write);
 
+                    // Skriv data til Cryptostream, krypter det og skriv til memoryStream.
                     cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
                     cryptoStream.FlushFinalBlock();
 
+                    // Konverter den krypterede memoryStream til et byte-array.
                     retur = memoryStream.ToArray();
                 }
             }
 
-
+            // Returner det krypterede byte-array.
             return (retur);
         }
 
+        // Metode til at udføre kryptering ved hjælp af AES-algoritmen.
         public byte[] AES(byte[] dataToEncrypt, byte[] key, byte[] iv)
         {
             byte[] retur;
+            // Opret en instans af AES-krypteringsalgoritmen.
             using (var aes = Aes.Create())
             {
+                // Konfigurer krypteringsindstillingerne.
                 aes.Mode = CipherMode.CBC;
                 aes.Padding = PaddingMode.PKCS7;
 
                 aes.Key = key;
                 aes.IV = iv;
-
+                
+                // Opret en memoryStream til krypteret data.
                 using (var memoryStream = new MemoryStream())
                 {
+                    // Opret en cryptoStream baseret på AES-algoritmen.
                     var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(),
                         CryptoStreamMode.Write);
 
+                    // Skriv data til Cryptostream, krypter det og skriv til memoryStream.
                     cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
                     cryptoStream.FlushFinalBlock();
 
+                    // Konverter den krypterede memoryStream til et byte-array.
                     retur = memoryStream.ToArray();
                 }
             }
 
-
+            // Returner det krypterede byte-array.
             return (retur);
         }
     }
