8b479e1e4454e2de780994df67e7e66093f763cc
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2023-08-09 18:17:31 +0200

    Update RandomGenerator.cs

--- symmetriskKrypteringØvelse/symmetriskKrypteringØvelse/RandomGenerator.cs
+++ symmetriskKrypteringØvelse/symmetriskKrypteringØvelse/RandomGenerator.cs
@@ -9,8 +9,10 @@ namespace symmetriskKrypteringØvelse
 {
     internal class RandomGenerator
     {
+        //generere et array af tilfældige bytes
         public byte[] GenerateRandomNumber(int length)
         {
+            // Længden af det genererede array angives som parameter til metoden.
             var randomNumberGenerator = RandomNumberGenerator.Create();
             byte[] randomBytes = new byte[length];
             randomNumberGenerator.GetBytes(randomBytes);
